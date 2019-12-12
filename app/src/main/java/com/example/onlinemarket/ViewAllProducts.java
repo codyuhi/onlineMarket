f2eab113e4f6dc17c4487405baa2087dee4d29b2
Cody Uhi <codylkuhi@gmail.com>
2019-12-11 18:25:45 -0700

    finished

--- app/src/main/java/com/example/onlinemarket/ViewAllProducts.java
+++ app/src/main/java/com/example/onlinemarket/ViewAllProducts.java
@@ -1,47 +1,72 @@
+// This file is used to view all products on screen at once
+// define the package
 package com.example.onlinemarket;
 
+// import the intent module to access intent extras and nav to other activities
 import android.content.Intent;
+// import asynctask for network calls to the api
 import android.os.AsyncTask;
+// import base android methods
 import android.os.Bundle;
+// import log module to log errors
 import android.util.Log;
+// import the view module to work with views
 import android.view.View;
+// import the progressbar for the progressbar
 import android.widget.ProgressBar;
+// import the textview module for textviews
 import android.widget.TextView;
+// import toast for user feedback
 import android.widget.Toast;
 
+// import appcompatactivity for easier coding
 import androidx.appcompat.app.AppCompatActivity;
+// import recyclerview widgets for the full list
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+// import floating action button for adding a new product
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+// snackbar was used in testing
 import com.google.android.material.snackbar.Snackbar;
 
+// import json array and object to work with json data
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+// import url to work with urls
 import java.net.URL;
+// import arraylist for the 
 import java.util.ArrayList;
 
+// begin class definition
 public class ViewAllProducts extends AppCompatActivity {
 
+// public and private variables/views for access across class
     private ProgressBar mLoadingProgress;
     public TextView noDataTextView;
     private RecyclerView rvFullList;
+    // intent gets the intent that led to this page loading
     public Intent intent = getIntent();
+    // auth will hold the user authentication info
     public static String auth;
 
+// This function is executed when the class is instantiated
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // the content view for this class is view_all_products.xml
         setContentView(R.layout.view_all_products);
 
+// define public/private variables and views that were initialized above
         mLoadingProgress = (ProgressBar) findViewById(R.id.mLoadingProgress);
         mLoadingProgress.setVisibility(View.INVISIBLE);
         noDataTextView = (TextView) findViewById(R.id.no_data);
         noDataTextView.setVisibility(View.INVISIBLE);
         intent = getIntent();
+        // if the token was passed from teh login screen, store that token here in the auth public var
         if(intent.getStringExtra("token") != null){
             auth = intent.getStringExtra("token");
         }
@@ -52,43 +77,58 @@ public class ViewAllProducts extends AppCompatActivity {
         LinearLayoutManager fullListLayoutManager = new LinearLayoutManager(this,
                 LinearLayoutManager.VERTICAL, false);
         rvFullList.setLayoutManager(fullListLayoutManager);
-
+// try/catch in case there are issues creating the fab or the intent to the new product page
         try {
+            // get the products
             new getProducts().execute("");
+            // create fab to create new product
             FloatingActionButton fab = findViewById(R.id.fab);
+            // create listener for the fab
             fab.setOnClickListener(new View.OnClickListener() {
+                // if the fab is pressed, this function runs
                 @Override
                 public void onClick(View view) {
-//                    Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-//                            .setAction("Action", null).show();
+                    // create intent and nav to the addproduct page
                     Intent addProductIntent = new Intent(ViewAllProducts.this, AddNewProduct.class);
                     startActivity(addProductIntent);
                 }
             });
         } catch (Exception e) {
+            // log any errors
             Log.d("Error: ", e.getMessage());
         }
     }
 
+// this function is used to execute the GET request to grab all the products
     public class getProducts extends AsyncTask<String, Void, String> {
+        // this runs in a background thread. used for the network call to API
         @Override
         protected String doInBackground(String... inputs){
             String result = null;
-
+// try/catch block to handle errors with api call
             try {
+                // build url for GET request for all products
                 URL getProductsUrl = ApiUtil.buildUrl("products");
+                // execute network call to API
                 result = ApiUtil.allProductsGET(getProductsUrl);
             } catch (Exception e) {
+                // log any errors
                 Log.d("Error: ", e.getMessage());
             }
+            // return the result of the GET request
             return result;
         }
 
+// This function runs when the network call is complete
         @Override
         protected void onPostExecute(String result) {
+            // hide the progress bar
             mLoadingProgress.setVisibility(View.INVISIBLE);
+            // the noDataTextView was used for debugging
             noDataTextView.setText(result);
+            // try catch in case of json errors or adapter errors
             try {
+                // if there was no response from the server, give user feedback
                 if(result == null){
 //                    noDataTextView.setVisibility(View.VISIBLE);
                     Toast.makeText(ViewAllProducts.this, "Unable to get product list data", Toast.LENGTH_LONG).show();
@@ -97,22 +137,26 @@ public class ViewAllProducts extends AppCompatActivity {
                     noDataTextView.setText(result);
 //                    noDataTextView.setVisibility(View.VISIBLE);
 
+// try catch for errors
                     try{
+                        // create array list for the recycler view
                         ArrayList<Product> fullListProducts = new ArrayList<Product>();
-
+                        // parse the result into a json object
                         JSONObject jsonFullList = new JSONObject(result);
+                        // grab the products from the json object and put them in a json array
                         JSONArray allProducts = jsonFullList.getJSONArray("products");
+                        // get length of json array
                         int productCount = allProducts.length();
+                        // create a new product array that will store data for all the products
                         Product[] products = new Product[productCount];
+                        // iterate through and do this for every element in the json array
                         for(int i =0; i < productCount; i++){
+                            // get all the attribute info from the json object
                             JSONObject productJson = allProducts.getJSONObject(i);
                             String nameJson = productJson.getString("name");
                             Float priceJson = Float.parseFloat(productJson.getString("price"));
                             String productImageJson = productJson.getString("productImage");
                             String _idJson = productJson.getString("_id");
-//                            JSONObject requestJson = productJson.getJSONObject("request");
-//                            String requestTypeJson = requestJson.getString("type");
-//                            String requestUrlJson = requestJson.getString("url");
                             String userIdJson = productJson.getString("userId");
                             String emailJson = productJson.getString("email");
                             Product productInfo = new Product (
@@ -123,29 +167,37 @@ public class ViewAllProducts extends AppCompatActivity {
                                     userIdJson,
                                     emailJson
                             );
+                            // store the new product in the product array
                             products[i] = productInfo;
+                            // add the new product into the array list
                             fullListProducts.add(productInfo);
                         }
 
                         try{
+                            // create a new adapter from the array list and set the adapter
                             ArrayList<Product> productArrayList = fullListProducts;
                             AllProductAdapter adapter = new AllProductAdapter(productArrayList);
                             rvFullList.setAdapter(adapter);
                         } catch (Exception e) {
+                            // log errors
                             Log.d("Error: ", e.getMessage());
                         }
                     } catch (Exception e) {
+                        // log errors
                         Log.d("Error: ", e.getMessage());
                     }
                 }
             } catch (Exception e) {
+                // log errors
                 Log.d("Error: ", e.getMessage());
             }
         }
 
+// This function runs before the network call is started
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
+            // show the progress bar
             mLoadingProgress.setVisibility(View.VISIBLE);
         }
     }
--- app/src/main/java/com/example/onlinemarket/ViewSingleProduct.java
+++ app/src/main/java/com/example/onlinemarket/ViewSingleProduct.java
@@ -1,23 +1,39 @@
+// This file is used to show a single product's info
+// Define the package
 package com.example.onlinemarket;
 
+// import the intent module to allow to nav to other pages and access intent extras
 import android.content.Intent;
+// import the asynctask for network calls to the api
 import android.os.AsyncTask;
+// import base android methods
 import android.os.Bundle;
+// import log to log errors
 import android.util.Log;
+// import view to work with views
 import android.view.View;
+// import button for buttons
 import android.widget.Button;
+// import progress bar for the progressbar
 import android.widget.ProgressBar;
+// import textview for the title
 import android.widget.TextView;
+// import toast for user feedback
 import android.widget.Toast;
 
+// import this for easier coding
 import androidx.appcompat.app.AppCompatActivity;
 
+// import json object to work with json
 import org.json.JSONObject;
 
+// import url to work with urls
 import java.net.URL;
 
+// begin class definition
 public class ViewSingleProduct extends AppCompatActivity {
 
+// create a bunch of public/private vars and views for use across class
     private ProgressBar mLoadingProgress;
     public TextView productName;
     public TextView productPrice;
@@ -28,11 +44,13 @@ public class ViewSingleProduct extends AppCompatActivity {
     public Button putButton;
     public String productId;
 
+// this function is executed when the class is instantiated
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_single_product);
 
+// define vars and views that were initialized earlier
         productName = (TextView) findViewById(R.id.productName);
         productPrice = (TextView) findViewById(R.id.productPrice);
         userId = (TextView) findViewById(R.id.userId);
@@ -42,37 +60,42 @@ public class ViewSingleProduct extends AppCompatActivity {
         deleteButton = (Button) findViewById(R.id.deleteButton);
         putButton = (Button) findViewById(R.id.editButton);
 
+// try getting the product
         try {
             new getProduct().execute("");
         } catch (Exception e) {
+            // log error
             Log.d("Error: ", e.getMessage());
         }
 
+// create event listener for the delete button
         deleteButton.setOnClickListener(new View.OnClickListener(){
+            // this function runs when the delete button is clicked
             public void onClick(View arg0) {
+            // try/catch in case the delete function throws error
                 try {
-                    try {
-                        new ViewSingleProduct.deleteFunction().execute("");
-                    } catch (Exception e) {
-                        Log.d("Error: ", e.getMessage());
-                    }
+                    // call delete function to delete product
+                    new ViewSingleProduct.deleteFunction().execute("");
                 } catch (Exception e) {
+                    // log errors
                     Log.d("Error: ", e.getMessage());
                 }
             }
         });
 
+// create event listener for the edit product button
         putButton.setOnClickListener(new View.OnClickListener(){
+            // this function runs when the edit product button is clicked
             public void onClick(View arg0) {
+            // try/catch in case the edit function throws error
                 try {
-                    try {
-                        Intent editIntent = new Intent(ViewSingleProduct.this, EditProduct.class);
-                        editIntent.putExtra("productId", productId);
-                        startActivity(editIntent);
-                    } catch (Exception e) {
-                        Log.d("Error: ", e.getMessage());
-                    }
+                    // nav to the edit product activity
+                    Intent editIntent = new Intent(ViewSingleProduct.this, EditProduct.class);
+                    // pass the product id to the edit activity
+                    editIntent.putExtra("productId", productId);
+                    startActivity(editIntent);
                 } catch (Exception e) {
+                    // log errors
                     Log.d("Error: ", e.getMessage());
                 }
             }
@@ -80,27 +103,34 @@ public class ViewSingleProduct extends AppCompatActivity {
 
     }
 
+// This function gets the product's info
     public class getProduct extends AsyncTask<String, Void, String> {
+        // This runs in background for network call to API
         @Override
         protected String doInBackground(String... input) {
             String result = null;
-
+            // Try catch for errors
             try {
-//                Intent intent = getIntent();
+                // grab the product id and execute GET request
                 String message = intent.getStringExtra("com.example.onlinemarket.MESSAGE");
                 URL getProductUrl = ApiUtil.buildUrl("products/" + message);
                 result = ApiUtil.singleProductGET(getProductUrl);
             } catch (Exception e) {
+                // log error
                 Log.d("Error: ", e.getMessage());
             }
-
+            // return result of the GET request
             return result;
         }
 
+// This function runs after the network call is complete
         @Override
         protected void onPostExecute(String result) {
+            // hide the loading bar
             mLoadingProgress.setVisibility(View.INVISIBLE);
+            // try catch for errors
             try {
+                // parse the response string into a json object
                 JSONObject jsonProduct = new JSONObject(result);
                 Product product = new Product(
                         jsonProduct.getString("_id"),
@@ -110,58 +140,72 @@ public class ViewSingleProduct extends AppCompatActivity {
                         jsonProduct.getString("userId"),
                         jsonProduct.getString("email")
                 );
+                // get the id from the json object
                 productId = jsonProduct.getString("_id");
+                // update the textviews with the product info
                 productName.setText(product.name);
                 productPrice.setText("Price: $" + product.price.toString());
                 userId.setText("User ID: " + product.userId);
                 seller.setText("Seller: " + product.email);
 
-//                Toast.makeText(ViewSingleProduct.this, intent.getStringExtra("auth"), Toast.LENGTH_LONG).show();
-//                Toast.makeText(ViewSingleProduct.this, ViewAllProducts.auth, Toast.LENGTH_LONG).show();
-
             } catch (Exception e) {
+                // log errors
                 Log.d("Error: ", e.getMessage());
             }
         }
 
+// This function runs before the network call starts
         @Override
         protected void onPreExecute(){
             super.onPreExecute();
+            // show the progress bar
             mLoadingProgress.setVisibility(View.VISIBLE);
         }
     }
 
+// This function deletes a product
     public class deleteFunction extends AsyncTask<String,Void,String> {
+        // This runs in the background to call network to api
         @Override
         protected String doInBackground(String... inputs){
             String result = null;
-
+            // try catch for errors
             try{
+                // grab the product id and pass it to the DELETE request
                 String message = intent.getStringExtra("com.example.onlinemarket.MESSAGE");
                 URL deleteUrl = ApiUtil.buildUrl("products/" + message);
                 result = ApiUtil.productDELETE(deleteUrl);
             } catch (Exception e){
+                // log error
                 Log.d("Error: ", e.getMessage());
             }
-
+// return result of DELETE request
             return result;
         }
 
+// This function runs after the network call is complete
         @Override
         protected void onPostExecute(String result) {
+            // hide the progress bar
             mLoadingProgress.setVisibility(View.INVISIBLE);
+            // if the delete op was successful,
             if(result.equals("OK")){
+                // provide user feedback
                 Toast.makeText(ViewSingleProduct.this, "Product Deleted Successfully", Toast.LENGTH_LONG).show();
+                // nav back to the view all products activity
                 Intent returnIntent = new Intent(ViewSingleProduct.this, ViewAllProducts.class);
                 startActivity(returnIntent);
             } else if(result.equals("Forbidden")){
+                // tell the user they don' have permission
                 Toast.makeText(ViewSingleProduct.this, "You do not have permission to delete this product.", Toast.LENGTH_SHORT).show();
             }
         }
 
+// This function runs before the network call is started
         @Override
         protected void onPreExecute(){
             super.onPreExecute();
+            // show the progress bar
             mLoadingProgress.setVisibility(View.VISIBLE);
         }
     }
